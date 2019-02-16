82f9108f31142b43cde28ae1253733b8b949175e
Sophie Welber <sjwelber@gmail.com>
2019-02-16 17:50:10 -0600

    function written to return list of 20% least frequent words

--- api/language_processing/word_freq.py
+++ api/language_processing/word_freq.py
@@ -7,11 +7,9 @@ class HMap:
     # map is the map
     def __init__(self, tokens):
         self.map = {}
-        self.orig_len = 0
         # go through JSON outputs and insert into map, increment original length
         for token in tokens:
             self.insert(token["text"]["content"])
-            self.orig_len += 1
 
     # inserts key if it doesn't exist and increments the count by 1
     def insert(self, key):
@@ -21,18 +19,19 @@ class HMap:
         else:
             self.map[key] += 1
     # returns a list of the least frequently appearing words in the text
-    # number of words returned is 20% of the original string length
-    def least_freq_percent_total(self):
-        least_freq = {}
-        size = self.orig_len * 0.2
+    # number of words returned is 20% of the number of unique words
+    def least_freq(self):
+        least_freq = []
+        size = len(self.map) * 0.2
+        print(size)
+        i = 0
         for key, value in sorted(self.map.items(), key=lambda (k,v): (v,k)):
-            print "%s: %s" % (key, value)
+            if (i > size):
+                break
+            least_freq.append((key, value))
+            i += 1
 
-    def least_freq_percent_unique(self):
-        return -1
-
-
-
+        return least_freq
 
 def main():
     tks = [
@@ -65,12 +64,65 @@ def main():
                 "text": {
                     "content": "At"
                     },
+            },
+            {
+                "text": {
+                    "content": "one"
+                    },
+            },
+            {
+                "text": {
+                    "content": "two"
+                    },
+            },
+            {
+                "text": {
+                    "content": "three"
+                    },
+            },
+            {
+                "text": {
+                    "content": "four"
+                    },
+            },
+            {
+                "text": {
+                    "content": "five"
+                    },
+            },
+            {
+                "text": {
+                    "content": "six"
+                    },
+            },
+            {
+                "text": {
+                    "content": "seven"
+                    },
+            },
+            {
+                "text": {
+                    "content": "eight"
+                    },
+            },
+            {
+                "text": {
+                    "content": "nine"
+                    },
+            },
+            {
+                "text": {
+                    "content": "ten"
+                    },
             }
+            
          ]
     my_map = HMap(tks)
     #for key in my_map.map:
         #print "(", key, ", ", my_map.map[key], ")"
-    my_map.least_freq_percent_total()
+    least_freq = my_map.least_freq()
+    for item in least_freq:
+        print(item)
 
 
 main()
