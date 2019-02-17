3e6ccf15c8b4e889430c6179a1d261d7dd05b783
Sophie Welber <sjwelber@gmail.com>
2019-02-16 23:58:25 -0600

    changed to correct localhost

--- website/public/pullRequest.js
+++ website/public/pullRequest.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  const URL = "http://localhost:8000/posts"
+  const URL = "http://localhost:8000/pasta_text"
 
   $.getJSON(URL, function(result){
     console.log("Data: " + result);
