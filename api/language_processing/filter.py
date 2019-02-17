a85ea668afff3df1097d131319978c55800ce45d
Sophie Welber <sjwelber@gmail.com>
2019-02-16 20:08:59 -0600

    skeleton for filtering functions

--- api/language_processing/filter.py
+++ api/language_processing/filter.py
@@ -1,6 +1,16 @@
-    # filters JSON to only include adverbs, adjectives, verbs, nouns, and numbers
-    # removes other stop words
-    # returns updated JSON file
-    def filter(data):
+# removes item from JSON
+def remove_item(data, item):
+    return -1
+
+
+# filters JSON to only include adverbs, adjectives, verbs, nouns, and numbers
+# removes other stop words
+# returns updated JSON file
+def filter_dumb(data):
+    return -1
+
+# filters JSON to only include words that are blanking candidates
+def filter_small(data, word_list):
+    return -1
 
 
