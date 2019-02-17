f3afac67c92bea3b9336472bfd52ad6672a8f135
Amy Huang <amyhuang@Amys-MacBook-Pro.local>
2019-02-16 19:24:12 -0600

    script adds text from JSON

--- website/public/script.js
+++ website/public/script.js
@@ -1,16 +1,30 @@
 var JSONdata = '{"data":{"partofspeech": ["VERB", "ADV", "NOUN"], "text":"I ran happily through the meadow"}}'; // example
-var data = JSON.parse(JSONdata);
+var alldata = JSON.parse(JSONdata);
+var posdata = alldata.data.partofspeech;
+var num_verbs = 0;
+var num_nouns = 0;
+var num_adjs = 0;
+var num_advs = 0;
+var num_nums = 0;
 
-
-var verbs = ["running", "walking", "swimming"];
-var nouns = ["runner", "walker", "swimmer"];
-var adjs = ["running", "walking", "swimming"];
-var advs = ["runningly", "walkingly", "swimmingly"];
-var nums = ["one run", "one walk", "one swim"];
-
-var i=0;
+var i;
+for(i=0; i<posdata.length; i++) {
+    pos = posdata[i];
+    if(pos.localeCompare("VERB") == 0) {
+        num_verbs++;
+    } else if(pos.localeCompare("NOUN") == 0) {
+        num_nouns++;
+    } else if(pos.localeCompare("ADJ") == 0) {
+        num_adjs++;
+    } else if(pos.localeCompare("ADV") == 0) {
+        num_advs++;
+    } else if(pos.localeCompare("NUM") == 0) {
+        num_nums++;
+    }
+}
 
 function createEntry(length, text, id) {
+    var i=0;
     for(i=0; i<length; i++) {
         var para = document.createElement("p");
         var node = document.createTextNode(text);
@@ -19,8 +33,8 @@ function createEntry(length, text, id) {
         element.appendChild(para);
     }
 }
-createEntry(verbs.length, "VERB", "verb")
-createEntry(nouns.length, "NOUN", "noun")
-createEntry(adjs.length, "ADJECTIVE", "adj")
-createEntry(advs.length, "ADVERB", "adv")
-createEntry(nums.length, "NUMBER", "num")
+createEntry(num_verbs, "VERB", "verb");
+createEntry(num_nouns, "NOUN", "noun");
+createEntry(num_adjs, "ADJECTIVE", "adj");
+createEntry(num_advs, "ADVERB", "adv");
+createEntry(num_nums, "NUMBER", "num");
