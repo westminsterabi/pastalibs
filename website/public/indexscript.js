171968bfe9ed2d2a058bc98cf2dff336b72beac2
Sophie Welber <sjwelber@gmail.com>
2019-02-17 10:18:11 -0600

    indexscript overrides default form submission

--- website/public/indexscript.js
+++ website/public/indexscript.js
@@ -1,11 +1,17 @@
-
-var formData = JSON.stringify($("myForm").serializeArray());
-
-$.ajax({
-  type: "POST",
-  url: "http://localhost:8000/pasta_text",
-  data: formData,
-  dataType: "json",
-  contentType : "application/json"
-});
+jQuery(document).on('ready', function() {
+  jQuery('form#add-new-task').bind('submit', function(event) {
+    
+    event.preventDefault();
+    
+    var formData = JSON.stringify($("myForm").serializeArray());
+    
+    $.ajax({
+      type: "POST", 
+      url: "http://localhost:8000/pasta_text",
+      data: formData,
+      dataType: "json",
+      contentType : "application/json"
+    });
+  });
+}
 
