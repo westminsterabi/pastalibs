775b526621226bd234bc9b0d1725a42424def4eb
Amy Huang <amyhuang@Amys-MacBook-Pro.local>
2019-02-16 21:46:00 -0600

    added a js script for the third page

---
+++ website/public/outputPagescript.js
@@ -0,0 +1,9 @@
+var JSONdata = '{"data":{"partofspeech": ["VERB", "ADV", "NOUN"], "text":"___ ran ___ through the ___"}}'; // example
+var alldata = JSON.parse(JSONdata);
+var textdata = alldata.data.text;
+
+var para = document.createElement("p")
+var node = document.createTextNode(textdata);
+para.appendChild(node);
+var element = document.getElementById("div1")
+element.appendChild(para)
