0255e9c2b9d099d96d5ef695374ecbfdc4d284ce
Sophie Welber <sjwelber@gmail.com>
2019-02-16 22:37:04 -0600

    filtering lists of words

--- api/language_processing/filter.py
+++ api/language_processing/filter.py
@@ -1,9 +1,34 @@
-filter(word_list):
-    size = len(word_list)
+import math
+import nltk
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+def filter_list(word_list):
+    return_size = math.floor(len(word_list) * 0.2)
+    new_list = []
+    # We only want these parts of speech
+    desired_pos = {"ADV", "ADJ", "VERB", "NOUN", "NUM"}
+    # remove undesired parts of speech and stop words
+    for word in word_list:
+        if word[1] in desired_pos and word[0] not in stop_words:
+            new_list.append(word)
+
+    return new_list[0:return_size]
+
+### for testing purposes
+# def main():
+#     words = {("cat", "NOUN"), ("dog", "NOUN"), ("the", "ADP"), ("although", "ADP"), ("mousey", "ADJ"), ("run", "VERB")}
+    
+#     print(words)
+#     words = filter(words)
+#     print(words)
+
+
+# main()
 
 
 
-    # filter out pos we don't want
-    # filter out stop words
+
+
 
 
